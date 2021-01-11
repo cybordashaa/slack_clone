4639c2ee805a759f5f6c9c9eaa9b1f7f412c1cf8
Dashaa <cybordashaa@gmail.com>
2021-01-11 21:31:01 +0800

    add(): set first channel on Page load, show active channel

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -11,6 +11,8 @@ class Channels extends React.Component {
     channelDetails: "",
     channelsRef: firebase.database().ref("channels"),
     modal: false,
+    firstLoad: true,
+    activeChannel: ''
   };
 
   componentDidMount() {
@@ -22,13 +24,26 @@ class Channels extends React.Component {
     let loadedChannels = [];
     this.state.channelsRef.on("child_added", (snap) => {
       loadedChannels.push(snap.val());
-      this.setState({ channels: loadedChannels });
+      this.setState({ channels: loadedChannels }, () => this.setFirstChannel());
     });
   };
+
+  setFirstChannel = () => {
+    const  firstChannel = this.state.channels[0];
+    if(this.state.firstLoad && this.state.channels.length > 0){
+      this.props.setCurrentChannel(firstChannel);
+      this.setActiveChannel(firstChannel);
+    }
+    this.setState({ firstLoad: false});
+  }
   changeChannel = channel => {
+    this.setActiveChannel(channel);
     this.props.setCurrentChannel(channel);
   }
 
+  setActiveChannel = channel => {
+    this.setState({activeChannel: channel.id})
+  }
   // display channel
   displayChannels = channels =>
   channels.length > 0 &&
@@ -38,6 +53,7 @@ class Channels extends React.Component {
       onClick={() => this.changeChannel(channel)}
       name={channel.name}
       style={{ opacity: 0.7 }}
+      active= {channel.id === this.state.activeChannel}
     >
       # {channel.name}
     </Menu.Item>
