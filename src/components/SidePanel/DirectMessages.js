0ec1368c78e755508aab7b6a42abec36f12f0076
Dashaa <cybordashaa@gmail.com>
2021-01-12 18:55:42 +0800

    add(): display active Directmessage channel

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -10,7 +10,8 @@ class DirectMessages extends React.Component {
       users: [],
       usersRef: firebase.database().ref("users"),
       connectedRef: firebase.database().ref(".info/connected"),
-      presenceRef: firebase.database().ref("presence")
+      presenceRef: firebase.database().ref("presence"),
+      activeChannel: ''
     };
   
     componentDidMount() {
@@ -76,14 +77,21 @@ class DirectMessages extends React.Component {
         };
         this.props.setCurrentChannel(channelData);
         this.props.setPrivateChannel(true);
+        this.activeChannel(user.uid);
     }
 
     getChannelId = userId => {
         const currentUserId = this.state.user.uid;
         return userId < currentUserId ? `${userId}/${currentUserId}` : `${currentUserId}/${userId}`; 
     }
+
+    activeChannel = userId => {
+        this.setState({
+            activeChannel: userId
+        })
+    }
     render() {
-      const { users } = this.state;
+      const { users, activeChannel } = this.state;
   
       return (
         <Menu.Menu className="menu">
@@ -96,6 +104,7 @@ class DirectMessages extends React.Component {
           {users.map(user => (
             <Menu.Item
               key={user.uid}
+              active={user.uid === activeChannel}
               onClick={() => this.changeChannel(user)}
               style={{ opacity: 0.7, fontStyle: "italic" }}
             >
